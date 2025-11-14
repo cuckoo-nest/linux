3a55e05301b70390684b615f4a32e452f906d065
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-11-13 19:48:45 -0600

    Fix build scripts and implement rndis configuration

--- build.sh
+++ build.sh
@@ -10,12 +10,21 @@ NEEDED_TOOLS=""
 DEF_CONFIG="$SCRIPT_DIR/configs/gtvhacker/defconfig"
 ROOTFS_PATH="$SCRIPT_DIR/rootfs/gtvhacker"
 LOGO_PATH="$SCRIPT_DIR/logo/nest-logo-320x320.png"
+BUILD_TEMP="$SCRIPT_DIR/build-temp"
+BUILD_KMOD="$BUILD_TEMP/modules"
+NEW_ROOTFS="$SCRIPT_DIR/initramfs_data.cpio"
+
+if [[ -d "$BUILD_TEMP" ]]; then
+  rm -rf "$BUILD_TEMP"
+fi
+
+mkdir -p "$BUILD_TEMP" "$BUILD_KMOD"
 
 if [[ -f "$1" ]]; then
   DEF_CONFIG="$1"
 fi
 
-if [[ -d "$2" ]]; then
+if [[ -e "$2" ]]; then
   ROOTFS_PATH="$2"
 fi
 
@@ -26,6 +35,16 @@ fi
 DEF_CONFIG="$(realpath "$DEF_CONFIG")"
 ROOTFS_PATH="$(realpath "$ROOTFS_PATH")"
 LOGO_PATH="$(realpath "$LOGO_PATH")"
+
+# Check for depmod
+if ! which depmod > /dev/null; then
+  NEEDED_TOOLS="$NEEDED_TOOLS kmod"
+fi
+
+# Check for fakeroot
+if ! which fakeroot > /dev/null; then
+  NEEDED_TOOLS="$NEEDED_TOOLS fakeroot"
+fi
 
 # Check for cpio
 if ! which cpio > /dev/null; then
@@ -55,21 +74,72 @@ source toolchain/bootstrap.sh
 # Create the logo file
 (
   echo "Converting \"$(basename "$LOGO_PATH")\" to \"logo_diamond_clut224.ppm\"..."
-	pngtopnm -mix "$LOGO_PATH" | \
-		  ppmquant -fs 223 | \
-		  pnmtoplainpnm > "$SCRIPT_DIR/linux/drivers/video/logo/logo_diamond_clut224.ppm"
+  pngtopnm -mix "$LOGO_PATH" | \
+    ppmquant -fs 223 | \
+    pnmtoplainpnm > "$SCRIPT_DIR/linux/drivers/video/logo/logo_diamond_clut224.ppm"
 )
 
 # Pack the rootfs cpio
-(
-  cd "$ROOTFS_PATH" || exit 1
-  find . -print0 | cpio -ov0 -H newc > "$SCRIPT_DIR/initramfs_data.cpio" || exit 1
-)
+#
+# Unpack with the following command in destination directory:
+# sudo cpio -H newc -ivdm --no-absolute-filenames -I "file_path.cpio"
+if [[ ! "$ROOTFS_PATH" == *".cpio" ]]; then
+  (
+    cd "$ROOTFS_PATH" || exit 1
+    FOLDERS=""
+
+    if [[ -f "./folders.txt" ]]; then
+      FOLDERS="$(cat ./folders.txt)"
+
+      cat ./folders.txt | while read folder; do
+        if [[ -n "$folder" ]]; then
+          mkdir -p "$folder" || exit 0
+        fi
+      done
+
+      rm ./folders.txt
+    fi
+    
+    fakeroot bash -c "cpio -H newc -ivdmu --no-absolute-filenames -I \"$SCRIPT_DIR/rootfs/devices.cpio\" && find . -print0 | LC_ALL=C sort -z | cpio -ov0 -H newc -O \"$NEW_ROOTFS\" || exit 1"
+    if [[ -n "$FOLDERS" ]]; then
+      echo "$FOLDERS" > ./folders.txt
+    fi
+  )
+else
+  cp "$ROOTFS_PATH" "$NEW_ROOTFS"
+fi
+
+ROOTFS_PATH="$NEW_ROOTFS"
 
 # Build the kernel
 (
   cd "$SCRIPT_DIR/linux" || exit 1
   make ARCH=arm "CROSS_COMPILE=$TOOLCHAIN_CROSS-" -j"$(nproc)" distclean || exit 1
   cp "$DEF_CONFIG" ".config"
-  make ARCH=arm "CROSS_COMPILE=$TOOLCHAIN_CROSS-" -j"$(nproc)" uImage || exit 1
+  echo "CONFIG_INITRAMFS_SOURCE=\"\"" >> .config
+  echo "CONFIG_RD_GZIP=y" >> .config
+  echo "CONFIG_INITRAMFS_ROOT_UID=0" >> .config
+  echo "CONFIG_INITRAMFS_ROOT_GID=0" >> .config
+  echo "CONFIG_INITRAMFS_COMPRESSION_NONE=y" >> .config
+  echo "CONFIG_INITRAMFS_COMPRESSION_GZIP=n" >> .config
+  echo "CONFIG_INITRAMFS_COMPRESSION_BZIP2=n" >> .config
+  echo "CONFIG_INITRAMFS_COMPRESSION_LZMA=n" >> .config
+  echo "CONFIG_INITRAMFS_COMPRESSION_LZO=n" >> .config
+
+  make ARCH=arm "CROSS_COMPILE=$TOOLCHAIN_CROSS-" "INSTALL_MOD_PATH=$BUILD_KMOD" -j"$(nproc)" all modules_install || exit 1
+  KVER="$(ls -1 "$BUILD_KMOD/lib/modules/")"
+
+  rm "$BUILD_KMOD/lib/modules/$KVER/build" "$BUILD_KMOD/lib/modules/$KVER/source"
+  
+  (
+    cd "$BUILD_KMOD" || exit 1
+    fakeroot bash -c "find . -print0 | LC_ALL=C sort -z | cpio -ov0 -H newc -AO \"$ROOTFS_PATH\" || exit 1"
+  )
+  
+  gzip -kf9 "$ROOTFS_PATH"
+  mkimage -A arm -O linux -T multi -C none \
+    -a 0x80008000 -e 0x80008000 \
+    -n "Linux+initramfs" \
+    -d "$SCRIPT_DIR/linux/arch/arm/boot/zImage:$ROOTFS_PATH.gz" \
+    "$SCRIPT_DIR/linux/arch/arm/boot/uImage"
 )
